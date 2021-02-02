1cf586bd6c59f29482d71229c50efa239b3610b3
szajberpiraty <szajberpiraty@gmail.com>
2021-02-02 16:34:48 +0100

    Input a billentyűzetről

---
+++ Valtozok/Input/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adatbevitel a billentyűzetről.
+
+            Console.Write("Adjon meg egy számot:");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adjon meg egy nem egész számot:");
+            double nemegesz = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine($"A begépelt értékek:{szam}|{nemegesz}");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
