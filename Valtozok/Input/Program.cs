7ef704dbe75720d211229c5d2764fad9376170f5
szajberpiraty <szajberpiraty@gmail.com>
2021-02-02 17:01:04 +0100

    Update Program.cs

--- Valtozok/Input/Program.cs
+++ Valtozok/Input/Program.cs
@@ -12,15 +12,37 @@ namespace Input
         {
             //Adatbevitel a billentyűzetről.
 
-            Console.Write("Adjon meg egy számot:");
-            int szam = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Adjon meg egy nem egész számot:");
-            double nemegesz = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Adjon meg egy számot:");
+            //int szam = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Adjon meg egy nem egész számot:");
+            //double nemegesz = Convert.ToDouble(Console.ReadLine());
 
 
 
-            Console.WriteLine($"A begépelt értékek:{szam}|{nemegesz}");
+            //Console.WriteLine($"A begépelt értékek:{szam}|{nemegesz}");
 
+            Console.Write("A:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            //Amikor egész számokat osztunk, az eredmény is egész lesz,
+            //még akkor is, van maradék
+            //"kényszeríthetjük" a műveletet arra, hogy a benne lévő 
+            //változók típusától eltérő típusú eredményt adjon vissza
+
+            //double c = (double)a / b;
+
+            //automatikus típus hozzárendelés a változóhoz a var 
+            //kulcsszóval
+
+            var c = a / b;
+
+
+
+
+
+            Console.WriteLine($"C={c}");
 
 
 
