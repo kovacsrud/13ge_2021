2853fd827b50d9957beb4801ede7e11182640a96
szajberpiraty <szajberpiraty@gmail.com>
2021-02-16 16:58:54 +0100

    Vezérlési szerkezetek

---
+++ VezerlesiSzerkezetek/VezerlesiSzerkezetek/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VezerlesiSzerkezetek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vezérlési szerkezetek
+            //elágazások
+            //ismételt tevékenységek (ciklusok)
+
+            string sz1 = "Kovács";
+            string sz2 = "kovács";
+
+            //Egyszeres egyágú
+            if (sz1==sz2)
+            {
+                Console.WriteLine("A nevek megegyeznek!");  
+            }
+
+            //Egyszeres kétágú elágazás (szelekció)
+            if (sz1==sz2)
+            {
+                Console.WriteLine("A nevek megegyeznek!");
+
+            } else
+            {
+                Console.WriteLine("A nevek nem egyeznek meg!");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
