6d32ceed791c5c116f9759e31bcf45c7db4cfb57
szajberpiraty <szajberpiraty@gmail.com>
2021-03-02 14:58:36 +0100

    Ciklusok

---
+++ Ciklusok/Ciklusok/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciklusok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciklus, iteráció, ismételt tevékenység
+
+            //előírt lépésszámú ciklus (for)
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //elöltesztelő ciklus
+
+
+            Console.ReadKey();
+        }
+    }
+}
