fb1516163660a19559a3f2b48ac88313f9b1cfde
szajberpiraty <szajberpiraty@gmail.com>
2021-03-02 17:37:03 +0100

    Nap vége

---
+++ Ciklusok/Kicsoda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kicsoda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            string[] nevek = {"Áron","Géza","Éva","Klára","Egon","Ella" };
+
+            Console.WriteLine("Ki vigye le a szemetet?");
+            int valasztas = rand.Next(nevek.Length);
+            Console.WriteLine($"A választott név indexe:{valasztas}");
+            Console.WriteLine($"A szemetet leviszi:{nevek[valasztas]}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
