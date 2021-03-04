14babd2ac69118a7971c46b3315fdf4c79ed0e72
szajberpiraty <szajberpiraty@gmail.com>
2021-03-04 15:23:15 +0100

    Listák

---
+++ Listak/Listak/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ez is azonos típusú elemeket tároló adatszerkezet.
+            //Az elemek számát nem kell megadni. 
+            List<int> szamok = new List<int>();
+
+            //elemek hozzáadása a listához
+            szamok.Add(123);
+            szamok.Add(34);
+            szamok.Add(69);
+
+            //A lista elemszámának meghatározása
+            Console.WriteLine(szamok.Count);
+
+            //Egy adott elem elérése a listában
+            Console.WriteLine(szamok[1]);
+            Console.WriteLine(szamok.ElementAt(2));
+            //A lista első elemének elérése
+            Console.WriteLine(szamok.First());
+
+            //A lista utolsó elemének elérése
+            Console.WriteLine(szamok.Last());
+            Console.WriteLine("------------------------");    
+            //A teljes lista kiíratása
+            for (int i = 0; i < szamok.Count; i++)
+            {
+                Console.WriteLine(szamok[i]);
+            }
+            Console.WriteLine("------------------------");
+
+            //A foreach ciklussal nem lehet módosítani a lista vagy tömb
+            //egy adott elemét!!
+            foreach (var i in szamok)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
