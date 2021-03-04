9035d96dcbfd210dd00aa02e03d83e5fea2c5ab3
szajberpiraty <szajberpiraty@gmail.com>
2021-03-04 14:37:15 +0100

    string manipulációk

---
+++ StringManipulacio/StringManipulacio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulacio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A string típus egy megváltoztathatatlan típus
+            //Ha új értéket adunk neki, akkor a háttérben egy új 
+            //változó jön létre
+
+            string szoveg = "ValAmI SzÖvEg";
+            //Változtassuk a betűk írásmódját a jelenlegivel ellentétesre
+            Console.WriteLine(szoveg);
+
+            char[] szovegch = szoveg.ToCharArray();
+
+            for (int i = 0; i < szovegch.Length; i++)
+            {
+                if (Char.IsLower(szovegch[i]))
+                {
+                    szovegch[i] = Char.ToUpper(szovegch[i]);
+                } else
+                {
+                    szovegch[i] = Char.ToLower(szovegch[i]);
+                }
+                //Console.Write(szovegch[i]);
+            }
+
+            szoveg = new string(szovegch);
+
+            Console.WriteLine(szoveg);
+
+            Console.ReadKey();
+        }
+    }
+}
