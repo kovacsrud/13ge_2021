7cd23b73a923f583f6f4cb92b49ba7b4434d0633
szajberpiraty <szajberpiraty@gmail.com>
2021-03-16 16:01:33 +0100

    Tippek bekérése

---
+++ Lotto/Lotto/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bekérjük, hogy hány számot sorsolunk (5,7..)
+            //Bekérjük, hogy hány számmal játszunk (45,90...)
+
+            //Tippek bekérése, ugyanaz a tipp nem szerepelhet 
+            //kétszer, a tippnek a megadott határok között
+            //kell lennie. Csak a megfelelő tipp kerülhet a
+            //tárolt tippek közé
+
+            //nyerőszámok sorsolása, véletlenszámokkal, ha olyan
+            //véletlen számot kapunk, ami már a nyerőszámok között van,
+            //akkor újat sorsolunk
+
+            //találatok számának meghatározása
+
+            //kiíratás
+            //Bekérés
+            Console.Write("Hány számot húzunk?:");
+            int szamDb = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Hány számból húzunk?:");
+            int osszSzam = Convert.ToInt32(Console.ReadLine());
+            
+            //Tömbök létrehozása
+            int[] tippek = new int[szamDb];
+            int[] nyeroSzamok = new int[szamDb];
+
+            //Tippelés
+            for (int i = 0; i < szamDb; i++)
+            {
+                Console.Write("Adja meg a tippet:");
+                int temp = Convert.ToInt32(Console.ReadLine());
+                while (temp<1 || temp>osszSzam || tippek.Contains(temp))
+                {
+                    Console.Write("Hibás tipp!Adja meg újra:");
+                    temp = Convert.ToInt32(Console.ReadLine());
+                }
+                tippek[i] = temp;
+
+            }
+            //Tippek listázása
+            for (int i = 0; i < tippek.Length; i++)
+            {
+                Console.Write(tippek[i]+" ");
+            }
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
