cb8e3c8c29082dba776bcd55b34d9fbf172b8e5c
szajberpiraty <szajberpiraty@gmail.com>
2021-04-13 16:24:32 +0200

    OOp alapok

---
+++ OOPAlapok/OOPAlapok/Ember.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAlapok
+{
+    class Ember
+    {
+        private string nev;
+        public int szuletesiev;
+        public int magassag;
+        public int suly;
+
+        //public void GetNev()
+        //{
+        //    Console.WriteLine($"A nevem:{nev}");
+        //}
+
+        public string GetNev()
+        {
+            return nev;
+        }
+
+        public void SetNev(string benev)
+        {
+            if (benev.Length>2)
+            {
+                nev = benev;
+            } else
+            {
+                nev = "alapérték";
+            }
+            
+        }
+
+    }
+}
---
+++ OOPAlapok/OOPAlapok/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAlapok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember ubul = new Ember();
+            ubul.magassag = 178;
+            ubul.SetNev("Ubul");
+            ubul.suly = 86;
+            ubul.szuletesiev = 1991;
+
+            Console.WriteLine(ubul.GetNev());
+
+            Ember elek = new Ember();
+            elek.magassag = 196;
+            elek.SetNev("Elek");
+            elek.suly = 69;
+            elek.szuletesiev = 1986;
+
+            Console.WriteLine(elek.GetNev());
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
