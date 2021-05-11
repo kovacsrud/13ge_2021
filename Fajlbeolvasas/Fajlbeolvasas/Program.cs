2adbe8b12f18cde70ed14a87d5f84d3012007fbe
szajberpiraty <szajberpiraty@gmail.com>
2021-05-11 15:05:49 +0200

    Szöveges fájl beolvasása

---
+++ Fajlbeolvasas/Fajlbeolvasas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fajlbeolvasas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream fajl = new FileStream(@"g:\snooker.txt", FileMode.Open);
+                StreamReader reader = new StreamReader(fajl,Encoding.Default);
+
+
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                }
+
+
+                reader.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
