574e24bcbb17369b14e9010cefcec668d9993d6d
szajberpiraty <szajberpiraty@gmail.com>
2021-05-25 17:16:41 +0200

    Adatfeldolgozás

---
+++ WpfHomerseklet/WpfAdatok/Versenyzo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAdatok
+{
+    public class Versenyzo
+    {
+        public int Helyezes { get; set; }
+        public string Nev { get; set; }
+        public string Orszag { get; set; }
+        public int Nyeremeny { get; set; }
+
+        public Versenyzo(string sor,char hatarolo)
+        {
+            var e = sor.Split(hatarolo);
+            Helyezes = Convert.ToInt32(e[0]);
+            Nev = e[1];
+            Orszag = e[2];
+            Nyeremeny = Convert.ToInt32(e[3]);
+        }
+    }
+}
---
+++ WpfHomerseklet/WpfAdatok/VersenyzoAdatok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAdatok
+{
+    public class VersenyzoAdatok
+    {
+        private List<Versenyzo> versenyzok;
+        public List<Versenyzo> Versenyzok { get { return versenyzok; }  }
+
+        public VersenyzoAdatok(string fajl,char hatarolo,int start=0)
+        {
+            versenyzok = new List<Versenyzo>();
+
+            var sorok = File.ReadAllLines(fajl, Encoding.Default);
+            for (int i = start; i < sorok.Length; i++)
+            {
+                versenyzok.Add(new Versenyzo(sorok[i],hatarolo));
+            }
+        }
+    }
+}
