9fb6db1119af82b0e163d0302088fafeef71b2b4
szajberpiraty <szajberpiraty@gmail.com>
2021-06-08 15:18:57 +0200

    Adatok WPF-ben

---
+++ WpfEladasok/WpfEladasok/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEladasok
+{
+    public class Game
+    {
+        public int Rank { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public string Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double GlobalSales { get; set; }
+
+        public Game(string sor,char hatarolo)
+        {
+            var e = sor.Split(hatarolo);
+
+            Rank = Convert.ToInt32(e[0]);
+            Name = e[1];
+            Platform = e[2];
+            Year = e[3];
+            Genre = e[4];
+            Publisher = e[5];
+            GlobalSales = Convert.ToDouble(e[6].Replace('.',','));
+        }
+
+    }
+}
---
+++ WpfEladasok/WpfEladasok/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEladasok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GameData gameData;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void buttonBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog()==true)
+            {
+                try
+                {
+                    gameData = new GameData(dialog.FileName,';',1);
+                    MessageBox.Show($"Betöltött adatsorok száma:{gameData.Games.Count}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.StackTrace,"Hiba!",MessageBoxButton.OK,MessageBoxImage.Error);                    
+                }
+            }
+
+        }
+    }
+}
